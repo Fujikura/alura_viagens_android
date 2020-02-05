9a8d77cabee90601d09d41a6a0bf18fe8ff30eb8
Marcio <marciofujikura@hotmail.com>
2020-02-05 09:09:39 -0300

    Bind e titulo app bar

--- app/src/main/java/br/com/alura/aluraviagens/ui/activity/PagamentoActivity.java
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/PagamentoActivity.java
@@ -3,15 +3,34 @@ package br.com.alura.aluraviagens.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
 
 import br.com.alura.aluraviagens.R;
+import br.com.alura.aluraviagens.model.Pacote;
+import br.com.alura.aluraviagens.util.MoedaUtil;
 
 public class PagamentoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Pagamento";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagamento);
-        setTitle("Pagamento");
+        setTitle(TITULO_APPBAR);
+
+        Pacote pacote = new Pacote
+                ("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
+
+        mostraPreco(pacote);
+    }
+
+    private void mostraPreco(Pacote pacote) {
+        TextView preco = findViewById(R.id.pagamento_valor_da_compra);
+        String moedaBrasileira = MoedaUtil.formataMoedaBrasileira(pacote.getPreco());
+        preco.setText(moedaBrasileira);
     }
 }
